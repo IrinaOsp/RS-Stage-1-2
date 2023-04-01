6ca1a1c06de59e24f5772e237672830ba8b41b90
“Irina <irina.osipova2012@gmail.com>
2023-04-01 15:06:58 +0200

    feat: no scroll for opened burger added

--- shelter/main.js
+++ shelter/main.js
@@ -3,22 +3,25 @@
     const burgerItem = document.querySelector('.header-burger');
     const menu = document.querySelector('.header-nav');
     const page = document.querySelector('.main');
+    const body = document.querySelector('.body');
     const menuLinks = document.querySelectorAll('.header-nav-link');
     burgerItem.addEventListener('click', () => {
         menu.classList.toggle('header-nav-active');
         burgerItem.classList.toggle('header-burger-active');
-        console.log('click')
+        body.classList.toggle('no-scroll');
     });
 
     page.addEventListener('click', () => {
         menu.classList.remove('header-nav-active');
         burgerItem.classList.remove('header-burger-active');
+        body.classList.remove('no-scroll');
     });
     if (window.innerWidth <= 767) {
         for (let i = 0; i < menuLinks.length; i++) {
             menuLinks[i].addEventListener('click', () => {
                 menu.classList.remove('header-nav-active');
                 burgerItem.classList.remove('header-burger-active');
+                body.classList.remove('no-scroll');
             });
         }
     }
