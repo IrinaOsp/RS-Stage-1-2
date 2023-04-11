77f2276ccce2cb496baa6bb76e49ccedb2ac2d1b
“Irina <irina.osipova2012@gmail.com>
2023-04-11 10:03:24 +0200

    fix: fix popup window positioning

--- shelter/main.js
+++ shelter/main.js
@@ -40,9 +40,7 @@ const BTNLEFT = document.querySelector('.slider-left');
 const CAROUSEL = document.getElementById('carousel');
 const PET_ITEMS = document.querySelectorAll('.friends-slider-item');
 
-const pastArr = [];
-const currArr = [];
-const nextArr = [];
+
 const PETS_NAMES = [];
     //get array of random numbers
 function getRandomNum(min, max) {
@@ -97,7 +95,7 @@ const PETS = [
     },
     {
       "name": "Woody",
-      "img": "../../assets/imgs/pets-woody.png",
+      "img": "./assets/imgs/pets-woody.png",
       "type": "Dog",
       "breed": "Golden Retriever",
       "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
@@ -108,7 +106,7 @@ const PETS = [
     },
     {
       "name": "Scarlett",
-      "img": "../../assets/imgs/pets-scarlet.png",
+      "img": "./assets/imgs/pets-scarlet.png",
       "type": "Dog",
       "breed": "Jack Russell Terrier",
       "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
@@ -119,7 +117,7 @@ const PETS = [
     },
     {
       "name": "Katrine",
-      "img": "../../assets/imgs/pets-katrine.png",
+      "img": "./assets/imgs/pets-katrine.png",
       "type": "Cat",
       "breed": "British Shorthair",
       "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
@@ -130,7 +128,7 @@ const PETS = [
     },
     {
       "name": "Timmy",
-      "img": "../../assets/imgs/pets-timmy.png",
+      "img": "./assets/imgs/pets-timmy.png",
       "type": "Cat",
       "breed": "British Shorthair",
       "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
@@ -141,7 +139,7 @@ const PETS = [
     },
     {
       "name": "Freddie",
-      "img": "../../assets/imgs/pets-freddie.png",
+      "img": "./assets/imgs/pets-freddie.png",
       "type": "Cat",
       "breed": "British Shorthair",
       "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
@@ -152,7 +150,7 @@ const PETS = [
     },
     {
       "name": "Charly",
-      "img": "../../assets/imgs/pets-charly.png",
+      "img": "./assets/imgs/pets-charly.png",
       "type": "Dog",
       "breed": "Jack Russell Terrier",
       "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
@@ -175,6 +173,20 @@ function getSliderItems() {
     })
 }
 getSliderItems()
+
+const pastArr = [];
+const currArr = [];
+const nextArr = [];
+
+
+let cardsNum
+if (window.innerWidth >= 1078) {
+    cardsNum = 3
+} else if (window.innerWidth < 1078 && window.innerWidth > 760) {
+    cardsNum = 2
+} else {
+    cardsNum = 1
+}
 
 /*
 const moveLeft = () => {
