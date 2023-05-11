44a67b189e41c29b0c374d11fb9defb68ac1a415
“Irina <irina.osipova2012@gmail.com>
2023-05-11 12:16:32 +0200

    feat: add project structure and webpack

---
+++ minesweeper/src/index.js
@@ -0,0 +1,7 @@
+import './index.html';
+import './style.css';
+import { mult, sum } from './modules/calc.js';
+
+//to be deleted
+console.log(mult(3, 4));
+console.log(sum(3, 4));
---
+++ minesweeper/src/modules/calc.js
@@ -0,0 +1,3 @@
+//created to check webpack, to be deleted
+export const mult = (a, b) => a * b;
+export const sum = (a, b) => a + b;
