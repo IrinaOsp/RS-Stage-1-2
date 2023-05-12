9efab5c5e8ec24c604a0900dc1358b2a00cc850e
“Irina <irina.osipova2012@gmail.com>
2023-05-12 15:21:10 +0200

    feat: add basic game info

--- minesweeper/src/modules/DOM-rendering.js
+++ minesweeper/src/modules/DOM-rendering.js
@@ -1,17 +1,52 @@
 export const CANVAS = document.createElement('canvas');
+
 // create basic layout
 export const createLayout = (function createLayout() {
   const WRAPPER = document.createElement('div');
   WRAPPER.className = 'wrapper';
 
+  const GAME_INFO = document.createElement('div');
+  GAME_INFO.className = 'game-info';
+
+  const SELECT_LEVEL = document.createElement('select');
+  SELECT_LEVEL.className = 'select';
+  SELECT_LEVEL.setAttribute('name', 'select level');
+
   CANVAS.className = 'canvas';
 
   document.body.appendChild(WRAPPER);
+  WRAPPER.appendChild(GAME_INFO);
+  GAME_INFO.appendChild(SELECT_LEVEL);
+
+  const gameLevelsArr = ['easy', 'medium', 'hard']
+  gameLevelsArr.forEach(lvl => {
+    const LEVEL = document.createElement('option');
+    LEVEL.className = 'level-option';
+    LEVEL.setAttribute('value', lvl);
+    LEVEL.innerText = lvl;
+    SELECT_LEVEL.appendChild(LEVEL);
+  })
+
+  const NEW_GAME_BTN = document.createElement('button');
+  NEW_GAME_BTN.className = 'button';
+  NEW_GAME_BTN.innerText = 'NEW GAME';
+  GAME_INFO.appendChild(NEW_GAME_BTN);
+
+  const TIMER = document.createElement('div')
+  GAME_INFO.appendChild(TIMER);
+
+  const timerArr = ['00', ':', '00'];
+  timerArr.forEach(el => {
+    const El = document.createElement('span');
+    El.className = 'time';
+    El.innerText = el;
+    TIMER.appendChild(El);
+  })
+
   WRAPPER.appendChild(CANVAS);
 }());
 
 export const ctx = document.querySelector('canvas').getContext('2d');
-
 const field = [];
 let bombs = [];
 
@@ -170,6 +205,11 @@ function showField (clickedCol, clickedRow) {
   }
   console.log(clickedCol, clickedRow);
 
+  if (field[clickedRow][clickedCol].hasBomb) {
+    ctx.fillStyle = "#f00";
+    ctx.fillText('*', clickedCol * CELL_SIZE + CELL_SIZE / 3, clickedRow * CELL_SIZE + 2 * CELL_SIZE / 3);
+    alert('Game over. Try again')
+  }
   // paint opened cells
   field[clickedRow][clickedCol].isOpen = true;
   if ((clickedCol + clickedRow) % 2 === 0) {
@@ -182,10 +222,6 @@ function showField (clickedCol, clickedRow) {
   if (field[clickedRow][clickedCol].bombCount > 0) {
     ctx.fillStyle = "#f00";
     ctx.fillText(`${field[clickedRow][clickedCol].bombCount}`, clickedCol * CELL_SIZE + CELL_SIZE / 3, clickedRow * CELL_SIZE + 2 * CELL_SIZE / 3);
-  }
-  if (field[clickedRow][clickedCol].hasBomb) {
-    ctx.fillStyle = "#f00";
-    ctx.fillText('*', clickedCol * CELL_SIZE + CELL_SIZE / 3, clickedRow * CELL_SIZE + 2 * CELL_SIZE / 3);
   }
 
   isFirstClick = false;
