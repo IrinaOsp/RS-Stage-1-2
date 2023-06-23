21749fe25ea194d180b3520ad43527020e8921f3
“Irina <irina.osipova2012@gmail.com>
2023-06-23 18:14:49 +0200

    feat: add appView

---
+++ RSS-CSS-Selectors/src/components/view/appView.ts
@@ -0,0 +1,20 @@
+import GamePanelDrawer from './game/gamePanel';
+import LevelPanel from './game/levels/levelsPanel';
+
+export class AppView {
+    private game: GamePanelDrawer;
+
+    private levelPanel: LevelPanel;
+
+    constructor() {
+        this.game = new GamePanelDrawer();
+        this.levelPanel = new LevelPanel();
+    }
+
+    public drawPage(): void {
+        this.game.draw();
+        this.levelPanel.highlightCurrentLvl();
+    }
+}
+
+export default AppView;
