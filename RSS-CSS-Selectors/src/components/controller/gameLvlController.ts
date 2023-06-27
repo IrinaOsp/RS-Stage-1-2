8ec8f5aa3ffbf6b03167b687a7dca4cdba9e64b5
“Irina <irina.osipova2012@gmail.com>
2023-06-27 15:22:07 +0200

    feat: win level add

--- RSS-CSS-Selectors/src/components/controller/gameLvlController.ts
+++ RSS-CSS-Selectors/src/components/controller/gameLvlController.ts
@@ -2,17 +2,27 @@ import DetectGameLvl from './gameLvlDetector';
 import LevelPanel from '../view/game/levels/levelsPanel';
 
 export default class LevelController {
-    public level: DetectGameLvl;
+    public level: string;
+
+    public completedLvls: string[] = [];
 
     constructor() {
-        this.level = new DetectGameLvl();
+        this.level = new DetectGameLvl().getDataFromLS();
+        if (localStorage.getItem('completed_levels')) {
+            this.completedLvls = JSON.parse(localStorage.getItem('completed_levels') as string);
+        } else {
+            this.completedLvls = [];
+        }
     }
 
     public changeLvlafterWin() {
-        if (Number(this.level.getDataFromLS()) >= 10) return;
-        const currentLevel = Number(this.level.getDataFromLS()) + 1;
+        if (Number(this.level) >= 10) return;
+        const currentLevel = Number(this.level) + 1;
+        this.completedLvls.push(this.level);
+        localStorage.setItem('completed_levels', JSON.stringify(this.completedLvls));
         localStorage.setItem('current_level', currentLevel.toString());
         const lvlPanel = new LevelPanel();
         lvlPanel.highlightCurrentLvl();
+        lvlPanel.checkCompletedLvls();
     }
 }
