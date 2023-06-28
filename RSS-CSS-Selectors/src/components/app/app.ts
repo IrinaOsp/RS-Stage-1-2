650e5de8367396a4b95a698c9d55a7bff16c911a
“Irina <irina.osipova2012@gmail.com>
2023-06-28 10:04:49 +0200

    feat: add buttons to app

--- RSS-CSS-Selectors/src/components/app/app.ts
+++ RSS-CSS-Selectors/src/components/app/app.ts
@@ -1,19 +1,28 @@
 import AppView from '../view/appView';
 import AppController from '../controller/appController';
+import Buttons from '../controller/buttons';
 
 class App {
     public view: AppView;
 
     public gameController: AppController;
 
+    public helpBtn: Buttons;
+
+    public resetBtn: Buttons;
+
     constructor() {
         this.view = new AppView();
         this.gameController = new AppController();
+        this.helpBtn = new Buttons('.help-btn');
+        this.resetBtn = new Buttons('.reset-btn');
     }
 
     public start(): void {
         this.view.drawPage();
         this.gameController.startGame();
+        this.helpBtn.addListener();
+        this.resetBtn.addListener();
     }
 }
 
