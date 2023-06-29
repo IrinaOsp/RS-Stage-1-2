5b9cd0d258152edf462f467610d254995c731e97
“Irina <irina.osipova2012@gmail.com>
2023-06-29 14:46:03 +0200

    feat: add emitter and lines draw

--- RSS-CSS-Selectors/src/components/view/HTMLview/htmlViewer.ts
+++ RSS-CSS-Selectors/src/components/view/HTMLview/htmlViewer.ts
@@ -1,4 +1,5 @@
 import levels from '../../data/levels';
+import eventEmitter from '../../controller/eventEmitter/eventEmitter';
 
 const hljs = require('highlight.js/lib/core');
 hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
@@ -6,23 +7,33 @@ hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
 export default class HTMLViewer {
     public view: HTMLDivElement | null;
 
+    public lines: HTMLUListElement | null;
+
     constructor() {
         this.view = document.querySelector('.HTML-code');
+        this.lines = document.querySelector('.code-lines-HTML');
     }
 
     drawView(level: number) {
         if (this.view) {
-            const highlightedCode = hljs.highlight(
-                `
-            <div class="grass">
-            ${levels[`level_${level}`].innerHTML}
-            </div>
-            `,
-                {
-                    language: 'xml',
-                }
-            ).value;
+            const str = `<div class="grass"> ${levels[`level_${level}`].innerHTML} </div>`;
+            const highlightedCode = hljs.highlight(str, { language: 'xml' }).value;
             this.view.innerHTML = highlightedCode;
+            let linesNumber = str.split('>').length;
+            if (this.lines?.hasChildNodes()) {
+                while (this.lines.firstChild) {
+                    this.lines.removeChild(this.lines.firstChild);
+                }
+            }
+            if (linesNumber <= 16) linesNumber = 16;
+            for (let i = 1; i < linesNumber; i += 1) {
+                const line = document.createElement('li');
+                line.textContent = i.toString();
+                this.lines?.appendChild(line);
+            }
         }
     }
 }
+
+const htmlPanel = new HTMLViewer();
+eventEmitter.on('levelChange', (newLevel) => htmlPanel.drawView(newLevel as number));
