3c10afe55384454f1944cb2c1d3f8c4358c6a8ce
“Irina <irina.osipova2012@gmail.com>
2023-07-03 22:39:59 +0200

    feat: add responsive design

--- RSS-CSS-Selectors/src/components/view/HTMLview/htmlViewer.ts
+++ RSS-CSS-Selectors/src/components/view/HTMLview/htmlViewer.ts
@@ -5,16 +5,16 @@ const hljs = require('highlight.js/lib/core');
 hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
 
 export default class HTMLViewer {
-    public view: HTMLDivElement | null;
+    private view: HTMLDivElement | null;
 
-    public lines: HTMLUListElement | null;
+    private lines: HTMLUListElement | null;
 
     constructor() {
         this.view = document.querySelector('.HTML-code');
         this.lines = document.querySelector('.code-lines-HTML');
     }
 
-    drawView(level: number) {
+    public drawView(level: number) {
         if (this.view) {
             const str = `<div class="grass"> ${levels[`level_${level}`].innerHTML} </div>`;
             const highlightedCode = hljs.highlight(str, { language: 'xml' }).value;
@@ -31,9 +31,45 @@ export default class HTMLViewer {
                 line.textContent = i.toString();
                 this.lines?.appendChild(line);
             }
+            this.formatView();
         }
+    }
+
+    public formatView() {
+        const tags: NodeListOf<Element> = document.querySelectorAll('.hljs-tag');
+        tags.forEach((el: Element, ind: number) => {
+            el.setAttribute('style', 'padding-left: 1rem;');
+            if (ind === 0 || ind === tags.length - 1) el.removeAttribute('style');
+        });
     }
 }
 
 const htmlPanel = new HTMLViewer();
-eventEmitter.on('levelChange', (newLevel) => htmlPanel.drawView(newLevel as number));
+eventEmitter.on('levelChange', (newLevel) => htmlPanel.drawView(Number(newLevel)));
+
+(function burger() {
+    const burgerItem: Element | null = document.querySelector('.header-burger');
+    const aside: Element | null = document.querySelector('.aside');
+    const wrapper: Element | null = document.querySelector('.wrapper');
+    const closeItem: Element | null = document.querySelector('.aside-close');
+    const asideLevels: NodeListOf<Element> = document.querySelectorAll('.game-lvl');
+    if (burgerItem && aside && wrapper && closeItem && asideLevels) {
+        burgerItem.addEventListener('click', () => {
+            console.log('burger-menu');
+            aside.classList.add('aside-active');
+        });
+        closeItem.addEventListener('click', () => {
+            aside.classList.remove('aside-active');
+        });
+        wrapper.addEventListener('click', () => {
+            aside.classList.remove('aside-active');
+        });
+        if (window.innerWidth <= 870) {
+            for (let i = 0; i < asideLevels.length; i += 1) {
+                asideLevels[i].addEventListener('click', () => {
+                    aside.classList.remove('aside-active');
+                });
+            }
+        }
+    }
+})();
