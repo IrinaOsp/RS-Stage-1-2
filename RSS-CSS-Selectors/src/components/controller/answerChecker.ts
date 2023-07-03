6fb8682cfd85e88d009626dd84b3826add9faba3
“Irina <irina.osipova2012@gmail.com>
2023-07-03 21:28:46 +0200

    feat: edit result check

--- RSS-CSS-Selectors/src/components/controller/answerChecker.ts
+++ RSS-CSS-Selectors/src/components/controller/answerChecker.ts
@@ -7,38 +7,50 @@ function handleAnimationEnd(event: AnimationEvent): void {
     }
 }
 
+function wrongAnswer(): void {
+    document.querySelectorAll('.animated').forEach((el) => el.classList.add('wrong'));
+}
+
 class AnswerChecker {
     public getInput(): void {
-        const input: HTMLInputElement | null = document.querySelector('.code-input');
+        console.log('getInput');
+        const input: HTMLTextAreaElement | null = document.querySelector('.code-input');
         if (input) input.addEventListener('keyup', this.checkResult);
         const enterButton: HTMLButtonElement | null = document.querySelector('.enter-button');
         if (enterButton) enterButton.addEventListener('click', this.checkResult);
     }
 
     private checkResult(event: Event): void {
+        console.log('checkResult');
         if ((event instanceof KeyboardEvent && event.key === 'Enter') || event instanceof MouseEvent) {
-            const input: HTMLInputElement | null = document.querySelector('.code-input');
+            const input: HTMLTextAreaElement | null = document.querySelector('.code-input');
             const text = input?.value;
+            console.log('checkResult');
             if (text) {
                 try {
                     const elements = document.querySelectorAll(text);
-                    console.log(document.querySelectorAll('.animated'));
-                    console.log(elements);
-                    if (elements) {
+                    if (elements.length) {
+                      console.log('if elements.length');
+                      console.log(elements);
+                      console.log(document.querySelectorAll('.animated'));
                         const isCorrect = Boolean([...elements].filter(el => el.classList.contains('animated')).length ===
-                        elements.length);
-                    if (isCorrect) {
-                      elements.forEach(el => el.classList.add('right'));
-                      //по оконч аним. запустить смену уровня
-                      const setNewLevel = new LevelController()
-                      setNewLevel.changeLvlafterWin();
+                        elements.length && [...elements].filter(el => el.classList.contains('animated')).length === document.querySelectorAll('.animated').length);
+                        console.log(isCorrect);
+                        if (isCorrect) {
+                          //по оконч аним. запустить смену уровня
+                          console.log('if correct');
+                          elements.forEach((el) => el.classList.add('right'));
+                          const setNewLevel = new LevelController();
+                          setTimeout(() => setNewLevel.changeLvlafterWin(), 300);
+                        } else {
+                          console.log('wrong');
+                          wrongAnswer();
+                        }
                     } else {
-                      console.log('wrong');
-                      document.querySelectorAll('.animated').forEach(el => el.classList.add('wrong'));
+                      wrongAnswer();
                     }
-                  }
-                  } catch (error) {
-                    document.querySelectorAll('.animated').forEach(el => el.classList.add('wrong'));
+                } catch (error) {
+                  wrongAnswer();
                 }
               document.querySelectorAll('.animated').forEach(el => el.addEventListener('animationend', handleAnimationEnd as EventListener));
             }
