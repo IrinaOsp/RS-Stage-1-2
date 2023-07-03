9634051b2735c4fe50b6a7ac91dbff41d5520328
“Irina <irina.osipova2012@gmail.com>
2023-07-03 21:54:13 +0200

    feat: edit level change

--- RSS-CSS-Selectors/src/components/controller/gameLvlController.ts
+++ RSS-CSS-Selectors/src/components/controller/gameLvlController.ts
@@ -8,22 +8,31 @@ export default class LevelController {
     public completedLvls: string[] = [];
 
     constructor() {
-        this.level = new DetectGameLvl().getDataFromLS();
-        if (localStorage.getItem('completed_levels')) {
-            this.completedLvls = JSON.parse(localStorage.getItem('completed_levels') as string);
-        } else {
-            this.completedLvls = [];
-        }
+        this.level = '1';
     }
 
     public changeLvlafterWin() {
+        console.log('changeLvlafterWin');
+        this.level = new DetectGameLvl().getDataFromLS();
+        console.log(this.level);
         if (Number(this.level) >= 10) return;
         const currentLevel = Number(this.level) + 1;
-        eventEmitter.emit('levelChange', currentLevel);
-        this.completedLvls.push(this.level);
+        console.log(currentLevel);
+
+        eventEmitter.emit('levelChange', currentLevel.toString());
+        if (localStorage.getItem('completed_levels')) {
+            this.completedLvls = JSON.parse(localStorage.getItem('completed_levels') as string);
+            console.log(this.completedLvls);
+        } else {
+            console.log('1');
+            this.completedLvls = [];
+        }
+        if (this.completedLvls.indexOf(this.level) === -1) this.completedLvls.push(this.level);
+        console.log(this.completedLvls);
         localStorage.setItem('completed_levels', JSON.stringify(this.completedLvls));
         localStorage.setItem('current_level', currentLevel.toString());
         const levelPanel = new LevelPanel();
         levelPanel.checkCompletedLvls();
+        console.log(currentLevel);
     }
 }
