bf9fdc573ff349c481bca83aec3779e85151f8d1
“Irina <irina.osipova2012@gmail.com>
2023-07-12 20:41:42 +0200

    feat: make draw garage fn depend from server data

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,8 +1,49 @@
 import './main.css';
-import drawGarage from './view/view_garage';
+import { drawGarageInputs, drawGarageCars } from './view/view_garage';
 import drawHeader from './view/view_header';
 import { drawMain } from './view/view_main';
+import { getCars } from './garage';
+import { getCarsResult } from './types/types';
+
+// , getCar, createCar, updateCar, deleteCar
 
 drawHeader();
 drawMain();
-drawGarage();
+drawGarageInputs();
+
+const getCarsData: () => void = async () => {
+  const allCars: getCarsResult = await getCars([
+    { key: '_page', value: 0 },
+    { key: '_limit', value: 6 },
+  ]);
+  console.log(allCars);
+  allCars.cars.forEach((car) => drawGarageCars(car.name, car.color));
+};
+getCarsData();
+// const main = async () => {
+//   const car = await getCar(2);
+//   console.log(car);
+// }
+// main();
+// const newCar = async () => {
+//   const car = await createCar({
+//     name: 'Tesla',
+//     color: '#fff',
+//   });
+//   console.log(car);
+// }
+// newCar();
+// const updatedCar = async () => {
+//   const car = await updateCar(5, {
+//     name: 'Lada',
+//     color: '#000',
+//   });
+//   console.log(car);
+// }
+// updatedCar();
+// const deletedCar = async () => {
+//   const car = await deleteCar(5);
+//   console.log(car);
+// }
+// deletedCar();
+// drawGarageCars();
--- async-race/src/view/view_garage.ts
+++ async-race/src/view/view_garage.ts
@@ -1,7 +1,7 @@
 import { MAIN, WRAPPER } from './view_main';
 import createButton from './view_buttons';
 
-const drawGarageInputs: () => void = () => {
+export const drawGarageInputs: () => void = () => {
   const CONTROL_BLOCK = document.createElement('div');
   CONTROL_BLOCK.classList.add('control-block');
   MAIN.prepend(CONTROL_BLOCK);
@@ -41,26 +41,29 @@ const drawGarageInputs: () => void = () => {
   BUTTON_GENERATE.addEventListener('click', () => console.log('generate cars'));
 };
 
-const drawGarageCars = () => {
+export const drawGarageCars = (name: string, color: string) => {
+  console.log('drawGarageCars');
   const CAR_CONTAINER = document.createElement('div');
   CAR_CONTAINER.className = 'car-container';
   WRAPPER.appendChild(CAR_CONTAINER);
+  const CONTAINER_HEAD = document.createElement('div');
+  CONTAINER_HEAD.className = 'car_subcontainer';
+  CAR_CONTAINER.append(CONTAINER_HEAD);
   const BUTTON_SELECT = createButton(CAR_CONTAINER, 'button-garage', 'select');
   const BUTTON_REMOVE = createButton(CAR_CONTAINER, 'button-garage', 'remove');
   const CAR_NAME = document.createElement('span');
   CAR_NAME.className = 'car-name';
-  CAR_NAME.textContent = 'Porshe';
+  CAR_NAME.textContent = name;
+  CONTAINER_HEAD.append(BUTTON_SELECT, BUTTON_REMOVE, CAR_NAME);
   const BUTTON_START = createButton(CAR_CONTAINER, 'button-move', 'a');
   const BUTTON_STOP = createButton(CAR_CONTAINER, 'button-move', 'b');
   const CAR = document.createElement('div');
   CAR.className = 'car';
-  CAR.setAttribute('backdround-image', '');
-  CAR_CONTAINER.append(BUTTON_SELECT, BUTTON_REMOVE, CAR_NAME, BUTTON_START, BUTTON_STOP, CAR);
+  // const img = document.createElement('img');
+  // img.src = imageSrc;
+  // img.alt = 'car';
+  // CAR.append(img);
+  // CAR.innerHTML = '<img src="../assets/car-hatchback-svgrepo-com.svg" alt="car">';
+  CAR.style.backgroundColor = color;
+  CAR_CONTAINER.append(BUTTON_START, BUTTON_STOP, CAR);
 };
-
-const drawGarage = () => {
-  drawGarageInputs();
-  drawGarageCars();
-}
-
-export default drawGarage;
