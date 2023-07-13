00908ad3521d94a920780e6b048a691612140109
“Irina <irina.osipova2012@gmail.com>
2023-07-13 13:34:37 +0200

    feat: add ev list for create, select, update btns

--- async-race/src/view/view_garage.ts
+++ async-race/src/view/view_garage.ts
@@ -1,62 +1,52 @@
 import { MAIN, WRAPPER } from './view_main';
-import createButton from './view_buttons';
+import { createElem } from './view_elements';
+import { HTMLTags } from '../types/types';
+import { drawAddedCar, updateCar } from './change_view/change cars';
 
 export const drawGarageInputs: () => void = () => {
-  const CONTROL_BLOCK = document.createElement('div');
-  CONTROL_BLOCK.classList.add('control-block');
-  MAIN.prepend(CONTROL_BLOCK);
+  const CONTROL_BLOCK = createElem(MAIN, HTMLTags.div, 'control-block');
+  const CREATE_NAME = createElem(CONTROL_BLOCK, HTMLTags.input, 'input-name create-name');
 
-  const CREATE_NAME = document.createElement('input');
-  CREATE_NAME.setAttribute('type', 'text');
-  CREATE_NAME.classList.add('input-name');
-  CONTROL_BLOCK.append(CREATE_NAME);
-
-  const CREATE_COLOR = document.createElement('input');
+  const CREATE_COLOR = createElem(CONTROL_BLOCK, HTMLTags.input, 'input-color create-color');
   CREATE_COLOR.setAttribute('type', 'color');
   CREATE_COLOR.setAttribute('value', '#ffffff');
-  CREATE_COLOR.classList.add('input-color');
-  CONTROL_BLOCK.append(CREATE_COLOR);
 
-  const BUTTON_CREATE = createButton(CONTROL_BLOCK, 'button-garage', 'create');
-  BUTTON_CREATE.addEventListener('click', () => console.log('create'));
+  const BUTTON_CREATE = createElem(CONTROL_BLOCK, HTMLTags.button, 'button-garage', 'create');
+  BUTTON_CREATE.addEventListener('click', () => drawAddedCar());
 
-  const UPDATE_NAME = document.createElement('input');
+  const UPDATE_NAME = createElem(CONTROL_BLOCK, HTMLTags.input, 'input-name update-name');
   UPDATE_NAME.setAttribute('type', 'text');
-  UPDATE_NAME.classList.add('input-name');
-  CONTROL_BLOCK.append(UPDATE_NAME);
 
-  const UPDATE_COLOR = document.createElement('input');
+  const UPDATE_COLOR = createElem(CONTROL_BLOCK, HTMLTags.input, 'input-color update-color');
   UPDATE_COLOR.setAttribute('type', 'color');
   UPDATE_COLOR.setAttribute('value', '#bcbcbc');
-  UPDATE_COLOR.classList.add('input-color');
-  CONTROL_BLOCK.append(UPDATE_COLOR);
 
-  const BUTTON_UPDATE = createButton(CONTROL_BLOCK, 'button-garage', 'update');
-  BUTTON_UPDATE.addEventListener('click', () => console.log('update'));
-  const BUTTON_RACE = createButton(CONTROL_BLOCK, 'button-garage button-yellow', 'race');
+  const BUTTON_UPDATE = createElem(CONTROL_BLOCK, HTMLTags.button, 'button-garage', 'update');
+  BUTTON_UPDATE.addEventListener('click', () => updateCar());
+  const BUTTON_RACE = createElem(CONTROL_BLOCK, HTMLTags.button, 'button-garage button-yellow', 'race');
   BUTTON_RACE.addEventListener('click', () => console.log('race'));
-  const BUTTON_RESET = createButton(CONTROL_BLOCK, 'button-garage button-yellow', 'reset');
+  const BUTTON_RESET = createElem(CONTROL_BLOCK, HTMLTags.button, 'button-garage button-yellow', 'reset');
   BUTTON_RESET.addEventListener('click', () => console.log('reset'));
-  const BUTTON_GENERATE = createButton(CONTROL_BLOCK, 'button-garage', 'generate cars');
+  const BUTTON_GENERATE = createElem(CONTROL_BLOCK, HTMLTags.button, 'button-garage', 'generate cars');
   BUTTON_GENERATE.addEventListener('click', () => console.log('generate cars'));
 };
 
 export const drawGarageCars = (name: string, color: string) => {
   console.log('drawGarageCars');
-  const CAR_CONTAINER = document.createElement('div');
-  CAR_CONTAINER.className = 'car-container';
-  WRAPPER.appendChild(CAR_CONTAINER);
-  const CONTAINER_HEAD = document.createElement('div');
-  CONTAINER_HEAD.className = 'car_subcontainer';
-  CAR_CONTAINER.append(CONTAINER_HEAD);
-  const BUTTON_SELECT = createButton(CAR_CONTAINER, 'button-garage', 'select');
-  const BUTTON_REMOVE = createButton(CAR_CONTAINER, 'button-garage', 'remove');
-  const CAR_NAME = document.createElement('span');
-  CAR_NAME.className = 'car-name';
-  CAR_NAME.textContent = name;
-  CONTAINER_HEAD.append(BUTTON_SELECT, BUTTON_REMOVE, CAR_NAME);
-  const BUTTON_START = createButton(CAR_CONTAINER, 'button-move', 'a');
-  const BUTTON_STOP = createButton(CAR_CONTAINER, 'button-move', 'b');
+  const CAR_CONTAINER = createElem(WRAPPER, HTMLTags.div, 'car-container');
+  const CONTAINER_HEAD = createElem(CAR_CONTAINER, HTMLTags.div, 'car_subcontainer');
+
+  const BUTTON_SELECT = createElem(CONTAINER_HEAD, HTMLTags.button, 'button-garage select-car', 'select');
+  BUTTON_SELECT.addEventListener('click', () => {
+    document.querySelectorAll('.selected').forEach((el) => el.classList.remove('.selected'));
+    BUTTON_SELECT.closest('.car-container')?.querySelector('.car')?.classList.add('selected');
+  });
+
+  const BUTTON_REMOVE = createElem(CONTAINER_HEAD, HTMLTags.button, 'button-garage remove-car', 'remove');
+  const CAR_NAME = createElem(CONTAINER_HEAD, HTMLTags.span, 'car-name', name);
+
+  const BUTTON_START = createElem(CAR_CONTAINER, HTMLTags.button, 'button-move', 'a');
+  const BUTTON_STOP = createElem(CAR_CONTAINER, HTMLTags.button, 'button-move', 'b');
   const CAR = document.createElement('div');
   CAR.className = 'car';
   // const img = document.createElement('img');
--- async-race/src/view/view_header.ts
+++ async-race/src/view/view_header.ts
@@ -1,14 +1,15 @@
-import createButton from './view_buttons';
+import { HTMLTags } from '../types/types';
+import { createElem } from './view_elements';
 
 const drawHeader: () => void = () => {
   const HEADER = document.createElement('header');
   HEADER.classList.add('header');
   document.body.append(HEADER);
 
-  const BUTTON_GARAGE = createButton(HEADER, 'button_header', 'to garage');
+  const BUTTON_GARAGE = createElem(HEADER, HTMLTags.button, 'button_header', 'to garage');
   BUTTON_GARAGE.addEventListener('click', () => console.log('to garage'));
 
-  const BUTTON_WINNERS = createButton(HEADER, 'button_header', 'to winners');
+  const BUTTON_WINNERS = createElem(HEADER, HTMLTags.button, 'button_header', 'to winners');
   BUTTON_WINNERS.addEventListener('click', () => console.log('to winners'));
 };
 
