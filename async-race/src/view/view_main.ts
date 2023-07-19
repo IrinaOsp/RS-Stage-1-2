68c227495d6f4f8d19a2a1f907c35782a08e0246
“Irina <irina.osipova2012@gmail.com>
2023-07-19 10:49:08 +0200

    refactor: use createElem func

--- async-race/src/view/view_main.ts
+++ async-race/src/view/view_main.ts
@@ -1,3 +1,6 @@
+import { HTMLTags } from '../types/types';
+import { createElem } from './view_elements';
+
 export const MAIN = document.createElement('main');
 export const WRAPPER = document.createElement('div');
 
@@ -8,22 +11,8 @@ export const drawMain: (cars: number, pages: number) => void = (cars, pages) => 
   WRAPPER.className = 'wrapper';
   MAIN.append(WRAPPER);
 
-  const GARAGE_HEADING = document.createElement('h2');
-  GARAGE_HEADING.className = 'garage-heading';
-  GARAGE_HEADING.textContent = 'Garage ';
-  const PAGE_HEADING = document.createElement('h3');
-  PAGE_HEADING.className = 'page-heading';
-  PAGE_HEADING.textContent = 'Page #';
-
-  WRAPPER.prepend(GARAGE_HEADING, PAGE_HEADING);
-
-  const CARS_COUNT = document.createElement('span');
-  CARS_COUNT.className = 'cars-count';
-  CARS_COUNT.textContent = `(${cars})`;
-  GARAGE_HEADING.appendChild(CARS_COUNT);
-
-  const PAGE_COUNT = document.createElement('span');
-  PAGE_COUNT.className = 'page-count';
-  PAGE_COUNT.textContent = `${pages}`;
-  PAGE_HEADING.appendChild(PAGE_COUNT);
+  const GARAGE_HEADING = createElem(WRAPPER, HTMLTags.h2, 'garage-heading', 'Garage ');
+  const PAGE_HEADING = createElem(WRAPPER, HTMLTags.h3, 'page-heading', 'Page #');
+  const CARS_COUNT = createElem(GARAGE_HEADING, HTMLTags.span, 'cars-count', `(${cars})`);
+  const PAGE_COUNT = createElem(PAGE_HEADING, HTMLTags.span, 'page-count', `${pages}`);
 };
